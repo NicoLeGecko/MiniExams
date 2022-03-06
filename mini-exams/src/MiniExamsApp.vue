b1552afabcc63c95a1b03553cc3bd9cd7eea82b4
Martineau Nicolas (Consultant) <nicolas.martineau@consultant.volvo.com>
2022-03-06 14:18:38 +0100

    edited starting template to start with a blank page

---
+++ mini-exams/src/MiniExamsApp.vue
@@ -0,0 +1,24 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <p>hi</p>
+  </header>
+
+  <main>
+  </main>
+</template>
+
+<style>
+@import './assets/miniExamsBase.css';
+
+#app {
+  color: brown;
+}
+
+header {
+  color: blueviolet;
+}
+
+</style>
